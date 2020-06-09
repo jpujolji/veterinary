1e9a6f39735e05601328f7586a1e878b1b122423
Jorge Pujol <jpujolji@gmail.com>
2020-06-09 18:45:40 -0500

    Commit inicial

---
+++ src/test/kotlin/com/codapps/veterinary/VeterinaryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codapps.veterinary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VeterinaryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
